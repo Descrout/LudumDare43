06808ed1c6b9976708672205a9bdc5276436ea95
Bahri SERTKAYA <bahrisertt@gmail.com>
2018-12-03 11:28:37 +0300

    null

--- source/ForBahri.hx
+++ source/ForBahri.hx
@@ -12,10 +12,10 @@ class ForBahri
 	public static inline var playerMaxVelX = 280;
 	public static inline var playerMaxVelY = 500;
 	public static inline var playerGravity = 980;
-	public static inline var playerFireRate = 1 / 10;
+	public static inline var playerFireRate = 1 / 5;
 	
-	public static inline var maxBullet = 6;
-	public static inline var playerBulletSpeed = 800;
+	public static inline var maxBullet = 60;
+	public static inline var playerBulletSpeed = 600;
 	public static inline var playerBulletDamage = 1;
 	
 	public static inline var crabRunSpeedMin = 180;
